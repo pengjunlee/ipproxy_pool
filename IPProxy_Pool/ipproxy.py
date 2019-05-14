e43c7fb0f2233ba7b60c96392b7fde00ce47835b
pengjunlee <34278256+pengjunlee@users.noreply.github.com>
2019-05-14 11:04:07 +0800

    Update ipproxy.py

--- IPProxy_Pool/ipproxy.py
+++ IPProxy_Pool/ipproxy.py
@@ -10,15 +10,14 @@ port_pattern = re.compile(r'^[0-9]{2,5}$', re.I)
 class IPProxy:
     '''
     {
-        "schema": "http",
-        "ip": "127.0.0.1",
-        "port": "8050",
-        "used_total": 11,
-        "success_times": 5,
-        "continuous_failed": 3,
-        "created_time": "2018-05-02"
+        "schema": "http", # 代理的类型
+        "ip": "127.0.0.1", # 代理的IP地址
+        "port": "8050", # 代理的端口号
+        "used_total": 11, # 代理的使用次数
+        "success_times": 5, # 代理请求成功的次数
+        "continuous_failed": 3, # 使用代理发送请求，连续失败的次数
+        "created_time": "2018-05-02" # 代理的爬取时间
     }
-
     '''
 
     def __init__(self, schema, ip, port, used_total=0, success_times=0, continuous_failed=0,
@@ -51,9 +50,18 @@ class IPProxy:
         ''' Return True if the proxy is https,otherwise return False'''
         return self.schema == 'https'
 
+    def _update(self, successed=False):
+        ''' Update proxy based on the result of the request's response'''
+        self.used_total = self.used_total + 1
+        if successed:
+            self.continuous_failed = 0
+            self.success_times = self.success_times + 1
+        else:
+            print(self.continuous_failed)
+            self.continuous_failed = self.continuous_failed + 1
 
 if __name__ == '__main__':
-    proxy = IPProxy('HTTPS', '1922.168.2.25', "8080")
+    proxy = IPProxy('HTTPS', '192.168.2.25', "8080")
     print(proxy._get_url())
     print(proxy._check_format())
     print(proxy._is_https())
